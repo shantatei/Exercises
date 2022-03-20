6cbb2d11809be5b47daab1e6909451863679cfdf
shantatei <teirenxuanshanta@gmail.com>
2022-03-20 21:40:08 +0800

    fixed some bugs

--- Session 2/tic-tac-toe/js/app.js
+++ Session 2/tic-tac-toe/js/app.js
@@ -1,231 +1,127 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const tiles = Array.from(document.querySelectorAll('.tile'));
-    const playerDisplay = document.querySelector('.display-player');
-    const restartButton = document.querySelector('#restart-btn');
-    const announcer = document.querySelector('.display-announcer');
-    const playerDisplayhide = document.querySelector('#player-display')
-    const gamelogsButton = document.querySelector('#logs-btn')
+const announcer = document.querySelector('#player-display');
+const numberOfTiles = 9;
 
+let boardstate;
+let board = ['', '', '', '', '', '', '', '', ''];
+let currentPlayer = 'X';
+let isGameActive = true;
 
+const PLAYERX_WON = 'Player X Won';
+const PLAYERO_WON = 'Player O Won';
+const TIE = 'Tie';
 
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 
+function changePlayer(player) {
+    currentPlayer = player ? player : currentPlayer === 'X' ? 'O' : 'X';
+    announcer.innerText = `Player ${currentPlayer}'s turn`;
+}
 
-    let boardstate;
-    let board = ['', '', '', '', '', '', '', '', ''];
-    let currentPlayer = 'X';
-    let isGameActive = true;
-
-
-
-    const PLAYERX_WON = 'PLAYERX_WON';
-    const PLAYERO_WON = 'PLAYERO_WON';
-    const TIE = 'TIE';
-
-
-    const winningConditions = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-    ];
-
-
-    //Checks for Win/Tie
-    function handleResultValidation() {
-        let roundWon = false;
-        for (let i = 0; i <= 7; i++) {
-            //winCondition refers to a scenario in which all 3 tiles are the same 
-            const winCondition = winningConditions[i];
-            //a = first tile 
-            const a = board[winCondition[0]];
-            //b = second tile 
-            const b = board[winCondition[1]];
-            //c = third  tile 
-            const c = board[winCondition[2]];
-            if (a === '' || b === '' || c === '') {
-                continue;
-            }
-            //If all 3 tiles are of the player (in this case X / Y)
-            if (a === b && b === c) {
-                //Set the roundWon to true
-                roundWon = true;
-                break;
-            }
+function handleResultValidation() {
+    let roundWon = false;
+    for (let i = 0; i < winningConditions.length; i++) {
+        //winCondition is possible way to win
+        const winCondition = winningConditions[i];
+        //getting the possible places in the board to check if they are the same to see if someone won
+        const a = board[winCondition[0]];
+        const b = board[winCondition[1]];
+        const c = board[winCondition[2]];
+        if (a === '' || b === '' || c === '') {
+            continue;
         }
-
-        if (roundWon) {
-            //Checks which player won depending on the currentPlayer var 
-            announce(currentPlayer === 'X' ? PLAYERX_WON : PLAYERO_WON);
-            isGameActive = false;
-            return;
-        }
-
-        // if all tiles have been filled up and roundWon is still false announce a Tie
-        if (!board.includes(''))
-            announce(TIE);
-    }
-
-
-    const saveLocalBoardState = (boardstate) => {
-        let historyboard = getHistoryBoard();
-        historyboard.push(boardstate);
-        localStorage.setItem('board', JSON.stringify(historyboard));
-    }
-
-    function getHistoryBoard() {
-        if (localStorage.getItem('board') === null) return [];
-        return JSON.parse(localStorage.getItem('board'));
-    }
-
-
-    const showboardHistory = () => {
-        let historyboardstate = JSON.parse(localStorage.getItem('board'));
-        const modalbody = document.querySelector('.modal-body');
-
-        for (let index = 0; index < historyboardstate.length; index++) {
-            //Creating Tic Tac Toe Grid
-            var tictactoe = `
-            <section class="display-announcer" item = "${index}"></section>
-            <section class="container-game-history">
-            <div class="tilehistory"><div item = "${index}"></div></div>
-            <div class="tilehistory"><div item = "${index}"></div></div>
-            <div class="tilehistory"><div item = "${index}"></div></div>
-            <div class="tilehistory"><div item = "${index}"></div></div>
-            <div class="tilehistory"><div item = "${index}"></div></div>
-            <div class="tilehistory"><div item = "${index}"></div></div>
-            <div class="tilehistory"><div item = "${index}"></div></div>
-            <div class="tilehistory"><div item = "${index}"></div></div>
-            <div class="tilehistory"><div item = "${index}"></div></div>
-            </section>`;
-            //Adding Grid
-            modalbody.insertAdjacentHTML('beforeend', tictactoe);
-
-            //Getting data for each game (OBJECT)
-            const pergame = historyboardstate[index]
-            
-            //Creating an array for the tile elements 
-            let tilearray = []
-            let tilehistoryarray = Array.from(document.querySelectorAll(`.tilehistory > [item="${index}"]`));
-            for (let index = 0; index < tilehistoryarray.length; index++) {
-                let parent = (tilehistoryarray[index]).parentElement
-                tilearray.push(parent)
-            }
-
-
-
-    
-            // Getting Announcer 
-            console.log(pergame.state)
-            let announcer = document.querySelector(`.display-announcer[item="${index}"]`)
-            announcer.innerHTML = `<span>${pergame.state ==  'PLAYERX_WON' ? "Player X Won" :  "Player O Won"}</span>`;
-
-            //Getting board array 
-            const boarditem = pergame.board
-
-
-            tilearray.forEach((tile, index) => {
-
-                tile.innerText = boarditem[index] //Trying to figure out how to get the data from board array to here !!
-            
-            })
+        if (a === b && b === c) {
+            roundWon = true;
+            break;
         }
     }
 
-
-    const announce = (type) => {
-        boardstate = {
-            board: board,
-            state: type
-        }
-        saveLocalBoardState(boardstate);
-        switch (type) {
-            case PLAYERO_WON:
-                announcer.innerHTML = 'Player <span class="playerO">O</span> Won';
-                playerDisplayhide.classList.add(`hide`);
-                playerDisplayhide.parentNode.replaceChild(announcer, playerDisplayhide)
-                break;
-            case PLAYERX_WON:
-                announcer.innerHTML = 'Player <span class="playerX">X</span> Won';
-                playerDisplayhide.classList.add(`hide`);
-                playerDisplayhide.parentNode.replaceChild(announcer, playerDisplayhide)
-
-                break;
-            case TIE:
-                announcer.innerText = 'Tie';
-                playerDisplayhide.classList.add(`hide`);
-                playerDisplayhide.parentNode.replaceChild(announcer, playerDisplayhide)
-
-        }
-        announcer.classList.remove('hide');
-    };
-
-    const isValidAction = (tile) => {
-        if (tile.innerText === 'X' || tile.innerText === 'O') {
-            return false;
-        }
-
+    if (roundWon) {
+        //Checks which player won depending on the currentPlayer var
+        announce(currentPlayer === 'X' ? PLAYERX_WON : PLAYERO_WON);
+        isGameActive = false;
         return true;
+    }
+
+    // if all tiles have been filled up and roundWon is still false announce a Tie
+    if (!board.includes('')) {
+        announce(TIE);
+        return true;
+    }
+
+    return false;
+}
+
+function announce(type) {
+    boardstate = {
+        board: board,
+        state: type
     };
-
-    const updateBoard = (index) => {
-        //updates the board array and set the tile 0-9 to either player X / Y depending on curentPlayer var 
-        board[index] = currentPlayer;
+    let historyboard = [];
+    if (localStorage.getItem('board') !== null) {
+        historyboard = JSON.parse(localStorage.getItem('board'));
     }
+    historyboard.push(boardstate);
+    localStorage.setItem('board', JSON.stringify(historyboard));
+    announcer.innerHTML = type;
+}
 
-    const changePlayer = () => {
-        playerDisplay.classList.remove(`player${currentPlayer}`);
-        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-        playerDisplay.innerText = currentPlayer;
-        playerDisplay.classList.add(`player${currentPlayer}`);
-    }
-
-
-
-
-    //User Action Function checks if the game is active & if the tile that they are clicking is already used 
-    const userAction = (tile, index) => {
-        if (isValidAction(tile) && isGameActive) {
-            //If true set inner text to X/O (CurrentPlayer)
+for (let i = 0; i < numberOfTiles; i++) {
+    let tile = document.createElement('div');
+    tile.classList.add('tile');
+    tile.addEventListener('click', () => {
+        if (tile.innerText === '' && isGameActive) {
             tile.innerText = currentPlayer;
-            tile.classList.add(`player${currentPlayer}`);
-            updateBoard(index);
-            handleResultValidation();
-            changePlayer();
+            board[i] = currentPlayer;
+            if (!handleResultValidation()) changePlayer();
         }
-    }
-
-    const resetBoard = () => {
-        board = ['', '', '', '', '', '', '', '', ''];
-        isGameActive = true;
-        announcer.classList.add('hide');
-        playerDisplayhide.classList.remove('hide')
-        announcer.parentNode.replaceChild(playerDisplayhide, announcer)
-
-
-        if (currentPlayer === 'O') {
-            changePlayer();
-        }
-
-        tiles.forEach(tile => {
-            tile.innerText = '';
-            tile.classList.remove('playerX');
-            tile.classList.remove('playerO');
-        });
-    }
-
-
-    //When User Click on Each Tile Call User Action Function
-    tiles.forEach((tile, index) => {
-        tile.addEventListener('click', () => userAction(tile, index));
     });
+    document.querySelector('.container-game').appendChild(tile);
+}
 
-    restartButton.addEventListener('click', resetBoard);
+document.querySelector('#restart-btn').addEventListener('click', () => {
+    if (board.every((item) => item == '')) return;
+    board = ['', '', '', '', '', '', '', '', ''];
+    isGameActive = true;
 
-    gamelogsButton.addEventListener('click', showboardHistory);
+    changePlayer('X');
 
+    document.querySelectorAll('.tile').forEach((tile) => {
+        tile.innerText = '';
+    });
+});
 
-})
+document.querySelector('#logs-btn').addEventListener('click', () => {
+    let historyboardstate = JSON.parse(localStorage.getItem('board'));
+    const modalbody = document.querySelector('.modal-body');
+    modalbody.innerHTML = '';
+
+    if (!historyboardstate || historyboardstate.length == 0)
+        return (modalbody.innerHTML =
+            'No game has been recorded. Try finishing a game to see it here');
+
+    historyboardstate.forEach((prevGame, index) => {
+        modalbody.insertAdjacentHTML(
+            'beforeend',
+            `<section class="display-announcer">${prevGame.state}</section>
+            <section class="container-game-history" item = "${index}"></section>`
+        );
+        const gameContainerHistory = document.querySelector(
+            `.container-game-history[item="${index}"]`
+        );
+        prevGame.board.forEach((tile) =>
+            gameContainerHistory.insertAdjacentHTML(
+                'beforeend',
+                `<div class = "tilehistory">${tile}</div>`
+            )
+        );
+    });
+});
